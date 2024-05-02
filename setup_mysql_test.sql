d1b3f1242b96db62fa642b4166c2f16491bb8bd1
VictorNalu <victorchinalurumogu@gmail.com>
2024-05-02 20:52:16 +0100

    console

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,7 @@
+-- Active: 1714647753297@@localhost@3306
 -- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
